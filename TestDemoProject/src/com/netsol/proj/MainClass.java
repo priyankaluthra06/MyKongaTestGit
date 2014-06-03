58ee8cff26f31d047decc65c74d313421732e542
priyanka.luthra <priyanka.luthra@MACH144.nsi.lab>
2014-06-03 12:30:27 +0530

    deleted second class change in comment.

--- TestDemoProject/src/com/netsol/proj/MainClass.java
+++ TestDemoProject/src/com/netsol/proj/MainClass.java
@@ -5,7 +5,7 @@ public class MainClass {
 	public static void main(String[] args)
 	{
 		System.out.println("Test File");
-		System.out.println("Test File Two");
+		System.out.println("Test 2");
 	}
 	
 }
--- TestDemoProject/src/com/netsol/proj/SecondClass.java
+++
@@ -1,5 +0,0 @@
-package com.netsol.proj;
-
-public class SecondClass {
-
-}
